ed23f78a40b72d91eec377857cc5914ea3dc5fc5
AndrewFlowe-rs <arnaldomahone@gmail.com>
2025-02-18 16:23:14 -0300

    correccion de errores

--- public/js/alfajor.js
+++ public/js/alfajor.js
@@ -1,13 +1,30 @@
-    window.onload = () => {
-        alert("No habia alfajores , pero toma chocolates :3");
-         const box = document.getElementById('box');
-    const fireworks = document.getElementById('fireworks');
-box.addEventListener("click", () => {
-        box.classList.toggle('open');
-        fireworks.style.opacity = box.classList.contains('open') ? '1' : '0';
-    });
-      
- 
-    }
+window.onload = () => {
+  alert("No habia alfajores , pero toma chocolates :3");
+  const box = document.getElementById("box");
+  const fireworks = document.getElementById("fireworks");
+  box.addEventListener("click", () => {
+    box.classList.toggle("open");
+    fireworks.style.opacity = box.classList.contains("open") ? "1" : "0";
+  });
+// Animación de escritura automática
+const frase = "'Si quieres ser mi estrella, prometo ser tu cielo.'";
+const poemaElement = document.querySelector(".poema");
 
+let index = 0;
+
+function escribirTexto() {
+  if (index < frase.length) {
+    poemaElement.textContent += frase.charAt(index);
+    index++;
+    setTimeout(escribirTexto, 100); // Velocidad de escritura (100ms por letra)
+  } else {
+    poemaElement.style.borderRight = "none"; // Quita el cursor al finalizar
+  }
+}
+
+escribirTexto(); // Inicia la animación de escritura
+};
+
+
+  
 
